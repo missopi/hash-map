e257b1ee22a1e30e32f25871466e3d580b0927e2
missopi <missopi@me.com>
2024-01-11 10:13:27 +0000

    Add functions needed for HashMap class

---
+++ lib/hashmap.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# class for hash map code
+class HashMap
+  # produce hash
+  def hash() end
+
+  # set hash
+  def set() end
+
+  # return key value
+  def get() end
+
+  # is key in hash map?
+  def key?() end
+
+  # delete from hash map
+  def remove() end
+
+  # number of keys in hash map
+  def length() end
+
+  # empies hash map
+  def clear() end
+
+  # returns array of all keys in hash map
+  def keys() end
+
+  # return array of values
+  def values() end
+
+  # returns array of all key, value pairs
+  def entries() end
+end
